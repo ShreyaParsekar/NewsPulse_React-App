8ef2217840a4644058c8d93f21903c180223e4d8
ShreyaParsekar <141206334+ShreyaParsekar@users.noreply.github.com>
2024-06-18 15:01:00 +0530

    changes made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar =() => {
       <div>
         <nav  className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
   <div  className="container-fluid">
-    <a  className="navbar-brand" href="/">NewsPulse</a>
+    <h4> NewsPulse</h4>
     <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span  className="navbar-toggler-icon"></span>
     </button>
